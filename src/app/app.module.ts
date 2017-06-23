c658761e59997abee6fdc2806521c66af4fc3347
Flavio Ferrara <thecrow.eric@gmail.com>
2017-06-23 13:49:53 -0300

    Application structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HeaderComponent } from './header/header.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
